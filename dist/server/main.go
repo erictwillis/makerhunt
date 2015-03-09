845d5a23cec3897e44ba2a1abf362a7d5f93975c
Remco <remco@dutchcoders.io>
2015-03-09 22:47:10 +0100

    updated build

--- dist/server/main.go
+++ dist/server/main.go
@@ -104,20 +104,20 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 		user.Email = settings.Email
 		user.Headline = settings.Headline
 		user.CreatedAt = time.Now()
-		user.Image = settings.Image
+		user.ImageUrl = settings.ImageUrl
 		user.ProfileUrl = settings.ProfileUrl
 		user.WebsiteUrl = settings.WebsiteUrl
 		user.PHSettings = settings
 		err = db.Users.Insert(&user)
 	} else if err != nil {
-		http.Error(w, err.Error(), 403)
+		http.Error(w, err.Error(), 500)
 		return
 	} else if err == nil {
 		// update settings with latest info
-		user.Image = settings.Image
+		user.ImageUrl = settings.ImageUrl
 		user.PHSettings = settings
 		if err = db.Users.UpdateId(user.UserId, &user); err != nil {
-			http.Error(w, err.Error(), 403)
+			http.Error(w, err.Error(), 500)
 			return
 		}
 	}
