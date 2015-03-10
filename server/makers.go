2bab5291ecabe27bf75eb1df408b92106799c9b7
Remco <remco@dutchcoders.io>
2015-03-10 10:57:16 +0100

    filter of maker data

--- server/makers.go
+++ server/makers.go
@@ -1,8 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/nlopes/slack"
+)
 
 func apiMakersAll(w http.ResponseWriter, r *http.Request) {
-	makers := cache.Makers()
+	makers := []slack.User{}
+
+	for _, maker := range cache.Makers() {
+		switch {
+		case maker.Deleted:
+			fallthrough
+		case maker.IsBot:
+			fallthrough
+		case maker.Profile.ImageOriginal == "":
+			continue
+		}
+
+		truncated_maker := slack.User{
+			Id: maker.Id,
+			Profile: slack.UserProfile{
+				Image192: maker.Profile.Image192,
+			},
+		}
+		makers = append(makers, truncated_maker)
+	}
+
 	WriteJSON(w, makers)
 }
