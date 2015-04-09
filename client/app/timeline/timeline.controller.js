6bb36405cc08cd2dc8dc5e52afd9cd3ee19d7e93
sleinadsanoj <jonas@ideahatch.co>
2015-04-09 19:00:52 +0200

    - timeline-comment section (initial) - timeline upcoming events card

--- client/app/timeline/timeline.controller.js
+++ client/app/timeline/timeline.controller.js
@@ -8,29 +8,30 @@ angular.module('makerhuntApp')
 
     Post.query(function (posts) {
         $scope.posts = posts;
+      console.log(posts);
     });
 
     $scope.canEdit = function(post) {
         return (false);
-    }
+    };
 
     $scope.canDelete = function(post) {
         return (true);
-    }
+    };
 
     $scope.edit = function(post) {
         alert("edit");
-    }
+    };
 
     $scope.delete = function(post) {
         post.$delete().then(function(post) {
             var index = $scope.posts.indexOf(post);
-            $scope.posts.splice(index, 1);     
+            $scope.posts.splice(index, 1);
         }).catch(function(e) {
             console.debug(e);
         }).finally(function() {
         });
-    }
+    };
 
     $scope.share = function(form) {
         if (form.$invalid)
