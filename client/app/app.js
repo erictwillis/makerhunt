9b089b5b124eea32ffc90df56c033235c71be66c
Remco <remco@dutchcoders.io>
2015-04-11 10:51:35 +0200

    * minified * removed abbreviations

--- client/app/app.js
+++ client/app/app.js
@@ -47,24 +47,6 @@ angular.module('makerhuntApp', [
 
     Angularytics.init();
 
-    moment.locale('en', {
-        relativeTime : {
-            future: "in %s",
-            past:   "%s ago",
-            s:  "sec.",
-            m:  "a min.",
-            mm: "%d min.",
-            h:  "an hour",
-            hh: "%d hours",
-            d:  "a day",
-            dd: "%d days",
-            M:  "a month",
-            MM: "%d months",
-            y:  "a year",
-            yy: "%d years"
-        }
-    });
-
     $rootScope.$on('$stateChangeStart', function (event, next) {
         Auth.isLoggedInAsync(function(loggedIn) {
             if (!angular.isDefined(next.roles)) {
