73179dd0eb059b5514b7424ef6647399f808aa28
sleinadsanoj <jonas@ideahatch.co>
2015-04-30 12:41:14 +0200

    -initial post layout implementation

--- client/app/post/post.controller.js
+++ client/app/post/post.controller.js
@@ -3,6 +3,7 @@
 angular.module('makerhuntApp')
     .controller('PostCtrl', function ($rootScope, $scope, $timeout, Post, Event, Auth, user, $stateParams) {
     $scope.post = {};
+    $scope.user = user;
 
     $timeout(function() {
         $scope.load();
@@ -13,6 +14,28 @@ angular.module('makerhuntApp')
             $scope.post = angular.extend($scope.post, post);
         }, function(error) {
         });
+    };
+
+    $scope.events = [];
+    $timeout(function() {
+      Event.query(function(data) {
+        angular.forEach(data, function(event) {
+          event = angular.extend(event, { till_date: moment(event.from_date).add(1,'hour') });
+          $scope.events.push(event);
+        });
+      });
+    });
+
+    $scope.eventsFilter = function(item) {
+      return (moment(item.from_date).isAfter(moment()));
+    };
+
+    $scope.sortEvents = function(event) {
+      return event.from_date;
+    };
+
+    $scope.sortComments = function(comment) {
+      return comment.created_at;
     };
 
 });
